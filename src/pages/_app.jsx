aab849a1f31981a0df5a0c46248848f43aa70487
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-09-01 17:46:58 +0200

    Footer

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,13 @@
 
 import "@/styles/globals.css";
 import { WaveSurferProvider } from "../context/waveSurferContext";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
     <WaveSurferProvider>
       <Component {...pageProps} />
-  
+  <Footer />
     </WaveSurferProvider>
   );
 }
