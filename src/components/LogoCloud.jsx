705e853b5b7dd1dab944ddc837b17a61e0d6de5e
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-09-07 21:52:25 +0200

    Logo cloud component

--- src/components/LogoCloud.jsx
+++ src/components/LogoCloud.jsx
@@ -1,53 +1,66 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+
+const logos = [
+  { src: "/images/logo-cloud/ableton-logo 1.png", alt: "Ableton logo" },
+  { src: "/images/logo-cloud/arturia-logo.png", alt: "Arturia logo" },
+  { src: "/images/logo-cloud/elektron-logo.png", alt: "Elektron logo" },
+  { src: "/images/logo-cloud/eventide-logo.png", alt: "Eventide logo" },
+  { src: "/images/logo-cloud/fabfilter-logo.png", alt: "FabFilter logo" },
+  { src: "/images/logo-cloud/izotope-logo.png", alt: "iZotope logo" },
+  { src: "/images/logo-cloud/logic-pro-logo.png", alt: "Logic Pro logo" },
+  { src: "/images/logo-cloud/moog-logo.png", alt: "Moog logo" },
+  {
+    src: "/images/logo-cloud/native-instruments-logo.png",
+    alt: "Native Instruments logo",
+  },
+  { src: "/images/logo-cloud/sequential-logo.png", alt: "Sequential logo" },
+  { src: "/images/logo-cloud/soundtoys-logo.png", alt: "Soundtoys logo" },
+  { src: "/images/logo-cloud/u-he-logo.png", alt: "U-He logo" },
+  {
+    src: "/images/logo-cloud/universal-audio-logo.png",
+    alt: "Universal Audio logo",
+  },
+  { src: "/images/logo-cloud/valhalla-logo.png", alt: "Valhalla logo" },
+];
 
 const LogoCloud = () => {
   return (
-    <section className="relative pt-32 sm:pt-48  pb-24 sm:pb-32">
-        <div className="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 justify-center overflow-hidden [mask-image:radial-gradient(50%_45%_at_50%_55%,white,transparent)]"></div>
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-center text-lg font-semibold leading-8 text-zinc-100">
-            Some of the awesome tools we use
-          </h2>
-          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-            <img
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
-              alt="Transistor"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-              alt="Reform"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
-              alt="Tuple"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
-              alt="SavvyCal"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
-              alt="Statamic"
-              width={158}
-              height={48}
-            />
-          </div>
-        </div>
-      </section>
-  )
-}
+    <section className="relative pt-32 sm:pt-48  pb-24 sm:pb-32 bg-zinc-950 ">
+      <div className="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 justify-center overflow-hidden [mask-image:radial-gradient(50%_45%_at_50%_55%,white,transparent)]"></div>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <h2 className="text-center text-xl font-semibold  text-zinc-100">
+          Some of the awesome tools we use
+        </h2>
 
-export default LogoCloud
+        <div className=" mt-10 w-full inline-flex flex-nowrap [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+          <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none   animate-infinite-scroll  ">
+            {logos.map((logo, i) => (
+              <li key={i} className=" " >
+                <img
+                  src={logo.src}
+                  alt={logo.alt}
+                  className=" w-40 max-h-12 object-scale-down grayscale opacity-60 hover:opacity-100 transition-opacity duration-300 "
+                />
+              </li>
+            ))}
+          </ul>
+
+          <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none  animate-infinite-scroll aria-hidden='true'  ">
+            {logos.map((logo, i) => (
+              <li key={i} className=" " >
+                <img
+                  src={logo.src}
+                  alt={logo.alt}
+                  className=" w-40 max-h-12 object-scale-down grayscale opacity-60 hover:opacity-100 transition-opacity duration-300 "
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LogoCloud;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     fontSize: {
-      xs: ['0.75rem', { lineHeight: '1rem' }],
-      sm: ['0.875rem', { lineHeight: '1.5rem' }],
-      base: ['1rem', { lineHeight: '1.75rem' }],
-      lg: ['1.125rem', { lineHeight: '1.75rem' }],
-      xl: ['1.25rem', { lineHeight: '2rem' }],
-      '2xl': ['1.5rem', { lineHeight: '2.25rem' }],
-      '3xl': ['1.75rem', { lineHeight: '2.25rem' }],
-      '4xl': ['2rem', { lineHeight: '2.5rem' }],
-      '5xl': ['2.5rem', { lineHeight: '3rem' }],
-      '6xl': ['3rem', { lineHeight: '3.5rem' }],
-      '7xl': ['4rem', { lineHeight: '4.5rem' }],
+      xs: ["0.75rem", { lineHeight: "1rem" }],
+      sm: ["0.875rem", { lineHeight: "1.5rem" }],
+      base: ["1rem", { lineHeight: "1.75rem" }],
+      lg: ["1.125rem", { lineHeight: "1.75rem" }],
+      xl: ["1.25rem", { lineHeight: "2rem" }],
+      "2xl": ["1.5rem", { lineHeight: "2.25rem" }],
+      "3xl": ["1.75rem", { lineHeight: "2.25rem" }],
+      "4xl": ["2rem", { lineHeight: "2.5rem" }],
+      "5xl": ["2.5rem", { lineHeight: "3rem" }],
+      "6xl": ["3rem", { lineHeight: "3.5rem" }],
+      "7xl": ["4rem", { lineHeight: "4.5rem" }],
     },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        'boucher': ['"Bouchers Sans 2.0"'],
-        'nunito': ['Nunito Sans', 'sans-serif']
+        boucher: ['"Bouchers Sans 2.0"'],
+        nunito: ["Nunito Sans", "sans-serif"],
+      },
+      animation: {
+        "infinite-scroll": "infinite-scroll 90s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-3136px)" },
+        },
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
