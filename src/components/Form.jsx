ef18d689426576a4079f93109888cc8d72b22a13
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-09-21 15:45:23 +0200

    Finish ghost production section UI

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,98 +1,96 @@
 
 const Form = () => {
   return (
-    <form action="#" method="POST" className="mt-16">
-      <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div>
-          <label
-            htmlFor="first-name"
-            className="block text-base font-semibold leading-6 text-zinc-100"
+    <form
+            action="#"
+            method="POST"
+            className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
           >
-            First name
-          </label>
-          <div className="mt-2.5">
-            <input
-              type="text"
-              name="first-name"
-              id="first-name"
-              autoComplete="given-name"
-              className="block w-full rounded-md border-0 px-3.5 py-2 text-zinc-100 shadow-sm ring-1 ring-inset ring-zinc-100/20 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 text-base bg-zinc-900"
-              required
-            />
-          </div>
-        </div>
-        <div>
-          <label
-            htmlFor="last-name"
-            className="block text-base font-semibold leading-6 text-zinc-100"
-          >
-            Last name
-          </label>
-          <div className="mt-2.5">
-            <input
-              type="text"
-              name="last-name"
-              id="last-name"
-              autoComplete="family-name"
-              className="block w-full rounded-md border-0 px-3.5 py-2 text-zinc-100 shadow-sm ring-1 ring-inset ring-zinc-100/20 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 text-base  bg-zinc-900"
-              required
-            />
-          </div>
-        </div>
-        <div className="sm:col-span-2">
-          <label
-            htmlFor="email"
-            className="block text-base font-semibold leading-6 text-zinc-100"
-          >
-            Email
-          </label>
-          <div className="mt-2.5">
-            <input
-              id="email"
-              name="email"
-              type="email"
-              autoComplete="email"
-              className="block w-full rounded-md border-0 px-3.5 py-2 text-zinc-100 shadow-sm ring-1 ring-inset ring-zinc-100/20 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 text-base  bg-zinc-900"
-              required
-            />
-          </div>
-        </div>
-        <div className="sm:col-span-2">
-          <div className="flex justify-between text-sm leading-6">
-            <label
-              htmlFor="message"
-              className="block text-base font-semibold leading-6 text-zinc-100"
-            >
-              Tell Us About Your Style
-            </label>
-          </div>
-          <div className="mt-2.5">
-            <textarea
-              id="message"
-              name="message"
-              rows={4}
-              aria-describedby="message-description"
-              className="block w-full rounded-md border-0 px-3.5 py-2 text-zinc-100 shadow-sm ring-1 ring-inset ring-zinc-100/20 placeholder:text-zinc-500 
-              placeholder:text-sm focus:ring-2 focus:ring-inset focus:ring-teal-600 text-base  bg-zinc-900"
-              defaultValue={""}
-              placeholder="Send us at least one reference track. Feel free to share the styles, other reference tracks, labels, or artists that inspire you. What specific elements do you love in your references? The more we know, the better we can bring your musical vision to life."
-              required
-            />
-          </div>
-        </div>
-      </div>
-      
-      <div className="mt-10 flex justify-end border-t border-gray-900/10 pt-8">
-        <button
-          type="submit"
-          className="rounded-md bg-blue-700 px-3.5 py-2.5 text-center text-base font-semibold text-zinc-100 shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700 w-full "
-        >
-          Send
-        </button>
-        
-      </div>
-      
-    </form>
+            <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
+              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div>
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-semibold leading-6 text-zinc-100"
+                  >
+                    First name
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="block w-full rounded-md border-0 bg-zinc-100/5 px-3.5 py-2 text-zinc-100 ring-1 ring-inset ring-zinc-100/10 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6 transition-all duration-300"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-semibold leading-6 text-zinc-100"
+                  >
+                    Last name
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="block w-full rounded-md border-0 bg-zinc-100/5 px-3.5 py-2 text-zinc-100 ring-1 ring-inset ring-zinc-100/10 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6 transition-all duration-300"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-semibold leading-6 text-zinc-100"
+                  >
+                    Email
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      autoComplete="email"
+                      className="block w-full rounded-md border-0 bg-zinc-100/5 px-3.5 py-2 text-zinc-100 ring-1 ring-inset ring-zinc-100/10 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6 transition-all duration-300"
+                    />
+                  </div>
+                </div>
+             
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="message"
+                    className="block text-sm font-semibold leading-6 text-white"
+                  >
+                    Message
+                  </label>
+                  <div className="mt-2.5">
+                    <textarea
+                      name="message"
+                      id="message"
+                      rows={4}
+                      className="block w-full rounded-md border-0 bg-zinc-100/5 px-3.5 py-2 text-zinc-100 shadow-sm ring-1 ring-inset ring-zinc-100/10 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6 transition-all duration-300"
+                      defaultValue={""}
+                      placeholder="Send us at least one reference track. Feel free to share the styles, other reference tracks, labels, or artists that inspire you. What specific elements do you love in your references? The more we know, the better we can bring your musical vision to life."
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mt-8 flex justify-end">
+                <button
+                  type="submit"
+                  className="flex-none rounded-xl px-3.5 py-2.5 text-sm font-semibold 
+                overflow-hidden group bg-blue-700  hover:bg-gradient-to-r hover:from-blue-700 hover:to-blue-600 text-zinc-100 hover:ring-2 hover:ring-offset-2 hover:ring-blue-600 transition-all ease-out duration-300 flex justify-center relative "
+                >
+                  <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+                  <span className="relative">Send message</span>
+                </button>
+              </div>
+            </div>
+          </form>
   );
 };
 
--- src/pages/ghost-production.jsx
+++ src/pages/ghost-production.jsx
@@ -4,18 +4,16 @@ import TimeLine from "@/components/TimeLine";
 import Image from "next/image";
 import HeroImage from "../../public/images/ghost-production-hero-image.png";
 
-
 import {
   BuildingOffice2Icon,
   EnvelopeIcon,
   PhoneIcon,
-  CheckIcon
+  CheckIcon,
 } from "@heroicons/react/24/outline";
 
 import { FadeIn, FadeInStagger } from "@/components/FadeIn";
 import { useState } from "react";
-
-
+import ContactImage from "../../public/images/ghost-production-contact-image.png";
 
 const features = [
   {
@@ -45,15 +43,14 @@ const features = [
 ];
 
 const pricingContent = [
-  '25 products',
-  'Up to 10,000 subscribers',
-  'Advanced analytics',
-  '24-hour support response time',
-  'Marketing automations',
-]
+  "25 products",
+  "Up to 10,000 subscribers",
+  "Advanced analytics",
+  "24-hour support response time",
+  "Marketing automations",
+];
 
 const GhostProduction = () => {
-
   return (
     <main className="  py-24 sm:py-32 relative">
       <FadeIn>
@@ -134,68 +131,57 @@ const GhostProduction = () => {
 
       <TimeLine />
 
-
-
       <section className=" py-24 sm:py-32 bg-gradient-radial from-teal-950/90 to-60% to to-transparent bg-bottom ">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-4xl text-center">
-        
-          <h2 className="mt-2 text-4xl font-bold tracking-tight text-zinc-100 sm:text-5xl">
-            Pricing
-          </h2>
-        </div>
-        <p className="mx-auto mt-6 max-w-2xl text-center text-lg text-gray-300">
-        Our price is transparent and fixed.<br/> Get started now, with no payment required and zero commitments..
-        </p>
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-4xl text-center">
+            <h2 className="mt-2 text-4xl font-bold tracking-tight text-zinc-100 sm:text-5xl">
+              Pricing
+            </h2>
+          </div>
+          <p className="mx-auto mt-6 max-w-2xl text-center text-lg text-gray-300">
+            Our price is transparent and fixed.
+            <br /> Get started now, with no payment required and zero
+            commitments..
+          </p>
 
-        <div className=" flex justify-center mt-10 px-6  ">
-         
-            <div
-             
-              className="bg-gradient-to-b from-zinc-950  to-zinc-900 ring-1 ring-zinc-100/10 rounded-3xl p-8 xl:p-10"
-            >
+          <div className=" flex justify-center mt-10 px-6  ">
+            <div className="bg-gradient-to-b from-zinc-950  to-zinc-900 ring-1 ring-zinc-100/10 rounded-3xl p-8 xl:p-10">
               <div className="flex items-center justify-center gap-x-4 ">
-                <h3  className="text-lg  font-semibold leading-8 text-zinc-100">
-                One original track
+                <h3 className="text-lg  font-semibold leading-8 text-zinc-100">
+                  One original track
                 </h3>
-             
               </div>
-              <p className="mt-4 text-sm leading-6 text-zinc-300 text-center ">Unique production crafted to your requirements</p>
+              <p className="mt-4 text-sm leading-6 text-zinc-300 text-center ">
+                Unique production crafted to your requirements
+              </p>
               <p className="mt-6  items-baseline gap-x-1 text-4xl font-bold tracking-tight text-zinc-100 text-center">
                 <span className="">1499 €</span>
-             
               </p>
               <a
-                      href="#contact-ghost-production"
-                      className=" mt-6 relative rounded-xl px-5 py-2.5 overflow-hidden group bg-blue-700  hover:bg-gradient-to-r hover:from-blue-700 hover:to-blue-600 text-zinc-100 hover:ring-2 hover:ring-offset-2 hover:ring-blue-600 transition-all ease-out duration-300 flex justify-center"
-                    >
-                      <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-48 bg-white opacity-10 rotate-12 sm:group-hover:-translate-x-80 ease"></span>
-                      <span className="relative">Get Started</span>
-                    </a>
-              <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10">
+                href="#contact-ghost-production"
+                className=" mt-6 relative rounded-xl px-5 py-2.5 overflow-hidden group bg-blue-700  hover:bg-gradient-to-r hover:from-blue-700 hover:to-blue-600 text-zinc-100 hover:ring-2 hover:ring-offset-2 hover:ring-blue-600 transition-all ease-out duration-300 flex justify-center"
+              >
+                <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-48 bg-white opacity-10 rotate-12 sm:group-hover:-translate-x-80 ease"></span>
+                <span className="relative">Get Started</span>
+              </a>
+              <ul
+                role="list"
+                className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10"
+              >
                 {pricingContent.map((feature) => (
                   <li key={feature} className="flex gap-x-3">
-                    <CheckIcon className="h-6 w-5 flex-none text-white" aria-hidden="true" />
+                    <CheckIcon
+                      className="h-6 w-5 flex-none text-white"
+                      aria-hidden="true"
+                    />
                     {feature}
                   </li>
                 ))}
               </ul>
             </div>
-      
+          </div>
         </div>
-      </div>
-    </section>
-
-
-
-
-
-
-
-
-
-
-
+      </section>
 
       {/* <section
         id="contact-ghost-production"
@@ -234,209 +220,54 @@ const GhostProduction = () => {
         </div>
       </section> */}
 
-      <div 
-      id="contact-ghost-production"
-      className="relative isolate bg-gray-900">
+      <div
+        id="contact-ghost-production"
+        className="relative isolate bg-zinc-950 "
+      >
+        {" "}
+        {/* <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden  ">
+          <div
+            className="absolute inset-0 h-full w-full  [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+            aria-hidden="true"
+          >
+            <Image
+              src={ContactImage}
+              className="object-cover w-full object-center -z-10"
+            />
+          </div>
+        </div> */}
         <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
           <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
-            <div 
-            
-            className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-              <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden ring-1 ring-white/5 lg:w-1/2">
-                <svg
-                  className="absolute inset-0 h-full w-full stroke-gray-700 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-                  aria-hidden="true"
-                >
-                  <defs>
-                    <pattern
-                      id="54f88622-e7f8-4f1d-aaf9-c2f5e46dd1f2"
-                      width={200}
-                      height={200}
-                      x="100%"
-                      y={-1}
-                      patternUnits="userSpaceOnUse"
-                    >
-                      <path d="M130 200V.5M.5 .5H200" fill="none" />
-                    </pattern>
-                  </defs>
-                  <svg
-                    x="100%"
-                    y={-1}
-                    className="overflow-visible fill-gray-800/20"
-                  >
-                    <path d="M-470.5 0h201v201h-201Z" strokeWidth={0} />
-                  </svg>
-                  <rect
-                    width="100%"
-                    height="100%"
-                    strokeWidth={0}
-                    fill="url(#54f88622-e7f8-4f1d-aaf9-c2f5e46dd1f2)"
-                  />
-                </svg>
-              </div>
-              <h2 className="text-3xl font-bold tracking-tight text-white">
-                Get in touch
+            <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
+              <h2 className="text-3xl font-bold tracking-tight text-zinc-100">
+                Get your free demo
               </h2>
-              <p className="mt-6 text-lg leading-8 text-gray-300">
-                Proin volutpat consequat porttitor cras nullam gravida at. Orci
-                molestie a eu arcu. Sed ut tincidunt integer elementum id sem.
-                Arcu sed malesuada et magna.
+              <p className="mt-6 text-lg leading-8 text-zinc-400">
+                Reach out to us in complete confidentiality to start your
+                project. Let’s work together to bring your unique sound to life!
               </p>
-              <dl className="mt-10 space-y-4 text-base leading-7 text-gray-300">
-                <div className="flex gap-x-4">
-                  <dt className="flex-none">
-                    <span className="sr-only">Address</span>
-                    <BuildingOffice2Icon
-                      className="h-7 w-6 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </dt>
-                  <dd>
-                    545 Mavis Island
-                    <br />
-                    Chicago, IL 99191
-                  </dd>
-                </div>
-                <div className="flex gap-x-4">
-                  <dt className="flex-none">
-                    <span className="sr-only">Telephone</span>
-                    <PhoneIcon
-                      className="h-7 w-6 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </dt>
-                  <dd>
-                    <a
-                      className="hover:text-white"
-                      href="tel:+1 (555) 234-5678"
-                    >
-                      +1 (555) 234-5678
-                    </a>
-                  </dd>
-                </div>
+              <dl className="mt-10 space-y-4 text-base leading-7 text-zinc-300">
                 <div className="flex gap-x-4">
                   <dt className="flex-none">
                     <span className="sr-only">Email</span>
                     <EnvelopeIcon
-                      className="h-7 w-6 text-gray-400"
+                      className="h-7 w-6 text-zinc-400"
                       aria-hidden="true"
                     />
                   </dt>
                   <dd>
                     <a
-                      className="hover:text-white"
-                      href="mailto:hello@example.com"
+                      className="hover:text-zinc-100"
+                      href="info@lap-studios.com"
                     >
-                      hello@example.com
+                      info@lap-studios.com
                     </a>
                   </dd>
                 </div>
               </dl>
             </div>
           </div>
-          <form
-            action="#"
-            method="POST"
-            className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
-          >
-            <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
-              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                <div>
-                  <label
-                    htmlFor="first-name"
-                    className="block text-sm font-semibold leading-6 text-white"
-                  >
-                    First name
-                  </label>
-                  <div className="mt-2.5">
-                    <input
-                      type="text"
-                      name="first-name"
-                      id="first-name"
-                      autoComplete="given-name"
-                      className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <label
-                    htmlFor="last-name"
-                    className="block text-sm font-semibold leading-6 text-white"
-                  >
-                    Last name
-                  </label>
-                  <div className="mt-2.5">
-                    <input
-                      type="text"
-                      name="last-name"
-                      id="last-name"
-                      autoComplete="family-name"
-                      className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-semibold leading-6 text-white"
-                  >
-                    Email
-                  </label>
-                  <div className="mt-2.5">
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      autoComplete="email"
-                      className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="phone-number"
-                    className="block text-sm font-semibold leading-6 text-white"
-                  >
-                    Phone number
-                  </label>
-                  <div className="mt-2.5">
-                    <input
-                      type="tel"
-                      name="phone-number"
-                      id="phone-number"
-                      autoComplete="tel"
-                      className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="message"
-                    className="block text-sm font-semibold leading-6 text-white"
-                  >
-                    Message
-                  </label>
-                  <div className="mt-2.5">
-                    <textarea
-                      name="message"
-                      id="message"
-                      rows={4}
-                      className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                      defaultValue={""}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="mt-8 flex justify-end">
-                <button
-                  type="submit"
-                  className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                >
-                  Send message
-                </button>
-              </div>
-            </div>
-          </form>
+          <Form />
         </div>
       </div>
     </main>
