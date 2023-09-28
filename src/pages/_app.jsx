4c2b8c86d09f6b9df97851af9689a57d9448d4dc
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-09-28 21:50:30 +0200

    Setup google analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,16 +6,31 @@ import { MDXProvider } from "@mdx-js/react";
 import MDXComponents from "@/components/MDXComponents";
 import { AudioProvider } from "@/context/audioContext";
 import BottomPLayer from "@/components/BottomPlayer";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <Providers>
       <AudioProvider>
         <MDXProvider components={MDXComponents}>
-            <Header />
-            <Component {...pageProps} />
-            <Footer />
-            <BottomPLayer />
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-1L4MQ4Q6JZ"
+          />
+          <Script
+          strategy="afterInteractive"
+          id="google-analytics">
+            {` 
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1L4MQ4Q6JZ');
+            `}
+          </Script>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+          <BottomPLayer />
         </MDXProvider>
       </AudioProvider>
     </Providers>
