85788d89709f88bc52493e97911d8348c26ed188
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-10-01 09:36:42 +0200

    Add google site verification on Home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,12 +6,13 @@ import ProjectFileFeatures from "@/components/ProjectFileFeatures";
 import LogoCloud from "@/components/LogoCloud";
 import Head from "next/head";
 
-
 export default function Home() {
   return (
     <main className=" bg-zinc-950 ">
       <Head>
-        <title>LAP Studios | Ghost Production Afro House, Melodic House & techno</title>
+        <title>
+          LAP Studios | Ghost Production Afro House, Melodic House & techno
+        </title>
         <meta
           name="description"
           content="Afro House, Melodic House & Techno, Organic House and Deep House ghost producers. Buy exclusive tracks to elevate your presence in the music industry."
@@ -25,6 +26,10 @@ export default function Home() {
           content="Afro House, Melodic House & Techno, Organic House and Deep House ghost producers. Buy exclusive tracks to elevate your presence in the music industry."
         />
         <meta property="og:image" content="/images/main-og-image.png" />
+        <meta
+          name="google-site-verification"
+          content="PEXYt6alIS4OjL6wIJ2DdLk4SnEG2tGKY4Aw2QWKo5g"
+        />
       </Head>
       <div className=" bg-zinc-950 ">
         <Hero />
